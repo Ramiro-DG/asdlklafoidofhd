9b12750ea5d60087b818a1df80f5cca84ddaba2e
Bruno Mollo <48107910+BrunoMollo@users.noreply.github.com>
2023-03-03 15:55:53 -0300

    Create script .sh

---
+++ script .sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo 'Este script deberia deployear cosas'
+
+curl https://archive.apache.org/dist/tomcat/tomcat-10/v10.0.13/bin/apache-tomcat-10.0.13.zip >apache-tomcat-10.0.13.zip
+
+unzip apache-tomcat-10.0.13.zip
+
+apk add openjdk11
+
+cd apache-tomcat-10.0.13/webapps
+
+curl -O https://github.com/Ramiro-DG/asdlklafoidofhd/raw/main/Farmacia_TP.war
+
+cd ~
+
+sh apache-tomcat-10.0.13/bin/catalina.sh run
